6e075a3c242fddf3f1bee4adff56099b4a4bc16e
K.Nakata <59306727+NakataKoo@users.noreply.github.com>
2024-10-12 00:39:34 +0900

    Update pretrain.sh

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,2 +1,7 @@
 export PYTHONPATH="."
-python3 MidiBERT/main.py --name=20241008_distilbert --datasets=lmd_aligned --model=distilbert --batch_size=32 --num_workers=16
+python3 MidiBERT/main.py \
+--name=20241008_distilbert \
+--datasets=lmd_aligned \
+--model=distilbert \
+--batch_size=32 \
+--num_workers=16
